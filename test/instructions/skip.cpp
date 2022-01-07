7d758d9f48d88507ef1662d428bf28172bb94416
Phillip O'Reggio <pnoreggio@gmail.com>
2022-01-07 00:52:24 -0500

    skip tests

--- test/instructions/skip.cpp
+++ test/instructions/skip.cpp
@@ -57,3 +57,59 @@ TEST(SkipNotEqualImmNoSkip, Skip) {
   doNcycles<2>(chip8);
   EXPECT_EQ(chip8.getPC(), ROM_START_ADDRESS + (INSTRUCTION_BYTE_SIZE * 3));
 }
+
+//
+// 5XY0: Skip if register VX and VY equal
+//
+
+TEST(SkipRegisterEqualSkip, Skip) {
+  auto chip8 = initChip8(romWithInstructions<5>({
+      0x6102, // load register 1 with 0x02
+      0x6202, // load register 2 with 0x02
+      0x5120, // Skip if register 1 == register 2
+      0x000F, // nop
+      0x000F, // nop
+  }));
+  doNcycles<3>(chip8);
+  EXPECT_EQ(chip8.getPC(), ROM_START_ADDRESS + (INSTRUCTION_BYTE_SIZE * 4));
+}
+
+TEST(SkipRegisterEqualNoSkip, Skip) {
+  auto chip8 = initChip8(romWithInstructions<5>({
+      0x6102, // load register 1 with 0x02
+      0x6204, // load register 2 with 0x04
+      0x5120, // Skip if register 1 == register 2
+      0x000F, // nop
+      0x000F, // nop
+  }));
+  doNcycles<3>(chip8);
+  EXPECT_EQ(chip8.getPC(), ROM_START_ADDRESS + (INSTRUCTION_BYTE_SIZE * 3));
+}
+
+//
+// 9XY0: Skip if register VX and VY not equal
+//
+
+TEST(SkipRegisterNotEqualSkip, Skip) {
+  auto chip8 = initChip8(romWithInstructions<5>({
+      0x6102, // load register 1 with 0x02
+      0x6203, // load register 2 with 0x03
+      0x9120, // Skip if register 1 != register 2
+      0x000F, // nop
+      0x000F, // nop
+  }));
+  doNcycles<3>(chip8);
+  EXPECT_EQ(chip8.getPC(), ROM_START_ADDRESS + (INSTRUCTION_BYTE_SIZE * 4));
+}
+
+TEST(SkipRegisterNotEqualNoSkip, Skip) {
+  auto chip8 = initChip8(romWithInstructions<5>({
+      0x6102, // load register 1 with 0x02
+      0x6202, // load register 2 with 0x02
+      0x9120, // Skip if register 1 != register 2
+      0x000F, // nop
+      0x000F, // nop
+  }));
+  doNcycles<3>(chip8);
+  EXPECT_EQ(chip8.getPC(), ROM_START_ADDRESS + (INSTRUCTION_BYTE_SIZE * 3));
+}
