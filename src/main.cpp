68319cfdc5051108ea979a5c91a0217e934566f2
pnor <pnoreggio@gmail.com>
2023-09-08 15:29:12 -0400

    handle interrupt more gracefully

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <signal.h>
 
 #include "chip8.hpp"
 #include "file_rom.hpp"
@@ -6,7 +7,23 @@
 #include "terminal_frontend.hpp"
 #include "terminal_input.hpp"
 
-int main() {
+void handleKeyboardInterrupt(int s) {
+  term_chip8::teardownDisplay();
+  exit(0);
+}
+
+void setupKeyboardInterruptHandler() {
+  // Handle keyboard interrupt
+  struct sigaction sigIntHandler;
+
+  sigIntHandler.sa_handler = handleKeyboardInterrupt;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+
+  sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+void emulateChip8() {
   term_chip8::setupDisplay();
 
   std::unique_ptr<IInput> input = std::make_unique<TerminalInput>();
@@ -24,4 +41,9 @@ int main() {
   }
 
   term_chip8::teardownDisplay();
+}
+
+int main() {
+  setupKeyboardInterruptHandler();
+  emulateChip8();
 }
